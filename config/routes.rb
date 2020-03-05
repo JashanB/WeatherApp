076e0fbc8307d5a5b76339a5457800cff3b51e25
JashanB <jashan_brar50@hotmail.com>
2020-03-05 16:37:25 -0500

    setup file structure

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'home#index'
+
+  resource :home, only: [:index]
+
   namespace :api do # /api/data
 
     get '/data', to: 'tests#index'
