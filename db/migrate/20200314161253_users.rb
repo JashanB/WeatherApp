a2daa3fdaa1b1fc56b87c6de253de211a38f6e08
JashanB <jashan_brar50@hotmail.com>
2020-03-14 12:25:05 -0400

    add users + places tables

---
+++ db/migrate/20200314161253_users.rb
@@ -0,0 +1,15 @@
+class Users < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users, force: :cascade do |t|
+      t.string "email"
+    end
+
+    create_table :places, force: :cascade do |t|
+      t.string "name"
+      t.decimal "latitude", null: false
+      t.decimal "longitude", null: false
+    end
+  
+    add_foreign_key :places, :users
+  end
+end
