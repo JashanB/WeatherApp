0bce2f1cb9a8c89a8462fdb5ecb45a1b0c176b31
JashanB <jashan_brar50@hotmail.com>
2020-03-14 13:52:21 -0400

    add models for user/place, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Seeding Data ..."
+
+puts "Creating points ..."
+
+User.destroy_all
+Place.destroy_all
+
+User.create(id: 1, email: "a@a")
+Place.create(id: 1, name: "Houston", latitude: 29.7604267, longitude: -95.3698028, user_id: 1)
+Place.create(id: 2,name: "Seattle", latitude: 47.6062095, longitude: 122.3320708, user_id: 1)
+
+
+
