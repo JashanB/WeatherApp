5978295fd51bcd0e17bde89fcd89e2e869b11cef
JashanB <jashan_brar50@hotmail.com>
2020-03-15 13:57:33 -0400

    fixed CORS issue

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'puma', '~> 3.11'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'excon'
 gem "figaro"
+gem 'rack-cors', :require => 'rack/cors'
 
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
--- config/application.rb
+++ config/application.rb
@@ -21,15 +21,21 @@ module RailsBackEnd
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
+    
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'http://localhost:3000'
+        resource '*', headers: :any, methods: [:get, :post, :put, :options, :delete, :show],
+        credentials: true
+      end
+    end
   end
 end
