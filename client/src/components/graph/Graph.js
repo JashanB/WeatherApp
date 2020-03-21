2e5748bfd644533216e2d90b3a7fc2b77bd59932
JashanB <jashan_brar50@hotmail.com>
2020-03-21 17:57:03 -0400

    add unit conversion

--- client/src/components/graph/Graph.js
+++ client/src/components/graph/Graph.js
@@ -8,132 +8,159 @@ const toCelsius = function (num) {
 	return temp
 }
 
+const convertTime = function(time) {
+	const date = new Date(time * 1000)
+	const formattedTime = date.toDateString();
+	const slicedTime = formattedTime.slice(4)
+	return slicedTime
+}
+
 export default (props) => {
 	let setNumber = props.pastYears
+	const current = props.weatherData.daily.data.map(function (day, index) {
+		const xValue = index + 1
+		const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
+		const dataObject = { x: xValueValue, y: averageTemp }
+		return dataObject
+	})
 	if (props.historicalData.hisWeather["1"]) {
 		const minus1 = props.historicalData.hisWeather["1"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 1, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
 	if (props.historicalData.hisWeather["2"]) {
 		const minus2 = props.historicalData.hisWeather["2"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 2, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
 	if (props.historicalData.hisWeather["3"]) {
 		const minus3 = props.historicalData.hisWeather["3"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 3, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
 	if (props.historicalData.hisWeather["4"]) {
 		const minus4 = props.historicalData.hisWeather["4"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 4, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
 	if (props.historicalData.hisWeather["5"]) {
 		const minus5 = props.historicalData.hisWeather["5"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 5, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
 	if (props.historicalData.hisWeather["6"]) {
 		const minus6 = props.historicalData.hisWeather["6"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 6, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
 	if (props.historicalData.hisWeather["7"]) {
 		const minus7 = props.historicalData.hisWeather["7"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 7, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
 	if (props.historicalData.hisWeather["8"]) {
 		const minus8 = props.historicalData.hisWeather["8"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 8, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
 	if (props.historicalData.hisWeather["9"]) {
 		const minus9 = props.historicalData.hisWeather["9"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 9, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
 	if (props.historicalData.hisWeather["10"]) {
 		const minus10 = props.historicalData.hisWeather["10"].map(function (day, index) {
+			const xValue = index + 1
 			const averageTemp = toCelsius((day.temperatureHigh + day.temperatureLow) / 2)
-			const dataObject = { x: 10, y: averageTemp }
+			const dataObject = { x: xValue, y: averageTemp }
 			return dataObject
 		})
 	}
-		const options = {
-			animationEnabled: true,
-			exportEnabled: true,
-			theme: "light2", // "light1", "dark1", "dark2"
-			title: {
-				text: "Bounce Rate by Week of Year"
-			},
-			axisY: {
-				title: "Bounce Rate",
-				includeZero: false,
-				suffix: "%"
-			},
-			axisX: {
-				title: "Week of Year",
-				prefix: "W",
-				interval: 2
-			},
-			data: [{
-				type: "line",
-				toolTipContent: "Week {x}: {y}%", //sets tool tip thing when hovering 
-				dataPoints: [
-					{ x: 1, y: 64 },
-					{ x: 2, y: 61 },
-					{ x: 3, y: 64 },
-					{ x: 4, y: 62 },
-					{ x: 5, y: 64 },
-					{ x: 6, y: 60 },
-					{ x: 7, y: 58 },
-					{ x: 8, y: 59 },
-					{ x: 9, y: 53 },
-					{ x: 10, y: 54 },
-					{ x: 11, y: 61 },
-					{ x: 12, y: 60 },
-					{ x: 13, y: 55 },
-					{ x: 14, y: 60 },
-					{ x: 15, y: 56 },
-					{ x: 16, y: 60 },
-					{ x: 17, y: 59.5 },
-					{ x: 18, y: 63 },
-					{ x: 19, y: 58 },
-					{ x: 20, y: 54 },
-					{ x: 21, y: 59 },
-					{ x: 22, y: 64 },
-					{ x: 23, y: 59 }
-				]
-			}]
-		}
-		return (
-			<div>
-				<CanvasJSChart options={options}
-				// {/* /* onRef={ref => this.chart = ref} */ */}
-				/>
-				{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-			</div>
-		);
-
+	const options = {
+		animationEnabled: true,
+		title: {
+			text: "Number of New Customers"
+		},
+		axisY: {
+			title: "Number of Customers",
+			includeZero: false
+		},
+		toolTip: {
+			shared: false
+		},
+		data: [{
+			type: "spline",
+			name: "2016",
+			showInLegend: true,
+			dataPoints: [
+				{ y: 155, label: "Jan" },
+				{ y: 150, label: "Feb" },
+				{ y: 152, label: "Mar" },
+				{ y: 148, label: "Apr" },
+				{ y: 142, label: "May" },
+				{ y: 150, label: "Jun" },
+				{ y: 146, label: "Jul" },
+				{ y: 149, label: "Aug" },
+				{ y: 153, label: "Sept" },
+				{ y: 158, label: "Oct" },
+				{ y: 154, label: "Nov" },
+				{ y: 150, label: "Dec" }
+			]
+		},
+		{
+			type: "spline",
+			name: "2017",
+			showInLegend: true,
+			dataPoints: [
+				{ y: 172, label: "Jan" },
+				{ y: 173, label: "Feb" },
+				{ y: 175, label: "Mar" },
+				{ y: 172, label: "Apr" },
+				{ y: 162, label: "May" },
+				{ y: 165, label: "Jun" },
+				{ y: 172, label: "Jul" },
+				{ y: 168, label: "Aug" },
+				{ y: 175, label: "Sept" },
+				{ y: 170, label: "Oct" },
+				{ y: 165, label: "Nov" },
+				{ y: 169, label: "Dec" }
+			]
+		}]
 	}
+	return (
+		<div>
+			<CanvasJSChart options={options}
+			// {/* /* onRef={ref => this.chart = ref} */ */}
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+	);
+
+}
